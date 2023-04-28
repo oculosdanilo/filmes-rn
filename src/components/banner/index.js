d175d5f93817fb04589f4f3e88adf0bcad27935f
Your Name <you@example.com>
2023-04-28 17:17:46 -0300

    04/28/2023 17:17:46

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Image, Text } from "react-native";
+import { Image, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
+
+const imagem = Math.floor(Math.random() * 4 + 1);
 
 export default function BannerMovies() {
   return (
     <>
       <Text style={styles.textBanner}>Em cartaz:</Text>
-      <Image source={require(`../../Img/2.jpg`)} style={styles.imageBanner} />
+      <Image source={require(`../../Img/${imagem}.jpg`)} style={styles.imageBanner} />
     </>
   );
 }
