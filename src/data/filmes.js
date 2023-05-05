b8737fa48507bc5b8afe9734ef1a23b9babdb6bb
Danilo Lima <danilo.lima124@etec.sp.gov.br>
2023-05-05 16:58:36 -0300

    05/05/2023 16:58:36

---
+++ src/data/filmes.js
@@ -0,0 +1,64 @@
+const DATA = [
+  {
+    id: "01",
+    nome: "Adão Negro",
+    nota: "4.5",
+    imagem: "adao.jpg",
+  },
+  {
+    id: "02",
+    nome: "Avatar",
+    nota: "6.2",
+    imagem: "avatar.jpg",
+  },
+  {
+    id: "03",
+    nome: "Desaparecida",
+    nota: "5",
+    imagem: "desaparecida.jpg",
+  },
+  {
+    id: "04",
+    nome: "Gato de Botas 2 - O Último Pedido",
+    nota: "7.1",
+    imagem: "gato.jpg",
+  },
+  {
+    id: "05",
+    nome: "Homem Formiga e a Vespa - Quantumania",
+    nota: "3.4",
+    imagem: "homemformiga.jpg",
+  },
+  {
+    id: "06",
+    nome: "Os Incríveis",
+    nota: "9.2",
+    imagem: "incriveis.jpg",
+  },
+  {
+    id: "07",
+    nome: "Super Mario Bros - O Filme",
+    nota: "5.2",
+    imagem: "mario.jpg",
+  },
+  {
+    id: "08",
+    nome: "M3GAN",
+    nota: "7.8",
+    imagem: "megan.jpg",
+  },
+  {
+    id: "09",
+    nome: "Panico VI",
+    nota: "9.1",
+    imagem: "panico.jpg",
+  },
+  {
+    id: "10",
+    nome: "Wandinha",
+    nota: "10",
+    imagem: "wandinha.jpg",
+  },
+];
+
+export default DATA;
