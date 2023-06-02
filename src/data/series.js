c712d0a12b39e4a88dd0c1fe81aeadba2a22bdbc
oculosdanilo <oculosdanilo@gmail.com>
2023-06-02 17:18:09 -0300

    06/02/2023 17:18:09

---
+++ src/data/series.js
@@ -0,0 +1,64 @@
+const DATA = [
+  {
+    id: "01",
+    nome: "Neon Genesis Evangelion",
+    nota: "10",
+    imagem: "eva.webp",
+  },
+  {
+    id: "02",
+    nome: "given",
+    nota: "10",
+    imagem: "dado.jpg",
+  },
+  {
+    id: "03",
+    nome: "Steven Universo",
+    nota: "10",
+    imagem: "stevenuniverso.webp",
+  },
+  {
+    id: "04",
+    nome: "Keeping Up With The Kardashians",
+    nota: "8",
+    imagem: "kuwtk.jpg",
+  },
+  {
+    id: "05",
+    nome: "Breaking Bad",
+    nota: "9",
+    imagem: "quebrandomal.jpg",
+  },
+  {
+    id: "06",
+    nome: "Cowboy Bebop",
+    nota: "10",
+    imagem: "cowboyhetero.jpg",
+  },
+  {
+    id: "07",
+    nome: "To Your Eternity",
+    nota: "9",
+    imagem: "fumetsu.jpg",
+  },
+  {
+    id: "08",
+    nome: "Free!",
+    nota: "7.8",
+    imagem: "megan.jpg",
+  },
+  {
+    id: "09",
+    nome: "Panico VI",
+    nota: "9.1",
+    imagem: "panico.jpg",
+  },
+  {
+    id: "10",
+    nome: "Wandinha",
+    nota: "10",
+    imagem: "wandinha.jpg",
+  },
+];
+
+export default DATA;
